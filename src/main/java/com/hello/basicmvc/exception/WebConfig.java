89fb4a23dc1f30066bf1f80dd04335df880d39c0
berryberry189 <berryberry189@naver.com>
2021-08-22 23:47:24 +0900

    서블릿 예외처리 - 인터셉터

--- src/main/java/com/hello/basicmvc/exception/WebConfig.java
+++ src/main/java/com/hello/basicmvc/exception/WebConfig.java
@@ -1,9 +1,11 @@
 package com.hello.basicmvc.exception;
 
 import com.hello.basicmvc.exception.filter.LogFilter;
+import com.hello.basicmvc.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -11,6 +13,14 @@ import javax.servlet.Filter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/*", "*.ico", "/error", "/error-page/**"); // 오류페이지 경로
+    }
 
     @Bean
     public FilterRegistrationBean logFilter(){
@@ -24,4 +34,20 @@ public class WebConfig implements WebMvcConfigurer {
 
         return filterRegistrationBean;
     }
+
+    /**
+     * [ /hello 정상 요청 ]
+     * WAS(/hello, dispatchType=REQUEST) -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러 -> View
+     * -------------------------------------------------------------------------------
+     *
+     * [ /error-ex 오류 요청 ]
+     * 필터는 DispatchType 으로 중복 호출 제거 ( dispatchType=REQUEST )
+     * 인터셉터는 경로 정보로 중복 호출 제거( excludePathPatterns("/error-page/**") )
+     * 1. WAS(/error-ex, dispatchType=REQUEST) -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러
+     * 2. WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
+     * 3. WAS 오류 페이지 확인
+     * 4. WAS(/error-page/500, dispatchType=ERROR) -> 필터(x) -> 서블릿 -> 인터셉터(x) -> 컨트롤러(/error-page/500) -> View
+     *
+     */
+
 }
