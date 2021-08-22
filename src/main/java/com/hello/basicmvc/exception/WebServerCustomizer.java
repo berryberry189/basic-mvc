1156598d6fb7f885623e35b6ac54141771a97a92
berryberry189 <berryberry189@naver.com>
2021-08-22 23:11:12 +0900

    서블릿 예외처리 - 오류페이지 작동 원리, 오류정보 출력

--- src/main/java/com/hello/basicmvc/exception/WebServerCustomizer.java
+++ src/main/java/com/hello/basicmvc/exception/WebServerCustomizer.java
@@ -8,6 +8,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    /**
+     * 예외 발생과 오류 페이지 요청 흐름
+     * 1. WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
+     * 2. WAS `/error-page/500` 다시 요청 -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러(/error- page/500) -> View
+     *                              ㄴ 오류 페이지 경로로 필터, 서블릿, 인터셉터, 컨트롤러가 모두 다시 호출
+     *  중요한 점은 웹 브라우저(클라이언트)는 서버 내부에서 이런 일이 일어나는지 전혀 모른다는 점이다.
+     *  오직 서버 내부에서 오류 페이지를 찾기 위해 추가적인 호출을 한다.
+     *
+     */
+
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
 
--- src/main/java/com/hello/basicmvc/exception/servlet/ServletExController.java
+++ src/main/java/com/hello/basicmvc/exception/servlet/ServletExController.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 public class ServletExController {
 
     // WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
-
     @GetMapping("/error-ex")
     public void errorEx(){
         throw  new RuntimeException("예외발생!");
