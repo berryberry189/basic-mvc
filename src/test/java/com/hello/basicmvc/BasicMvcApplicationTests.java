b2dfb232274bf13d0d698abb7597569b80eadae0
berryberry189 <berryberry189@naver.com>
2021-08-22 21:53:06 +0900

    Initial commit

---
+++ src/test/java/com/hello/basicmvc/BasicMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.basicmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
