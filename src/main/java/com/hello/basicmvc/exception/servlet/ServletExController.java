a4cecb4a94009d4982ffacf4cf2109b1299c5c9f
berryberry189 <berryberry189@naver.com>
2021-08-22 22:23:03 +0900

    서블릿 예외처리 - 예외발생, sendError

---
+++ src/main/java/com/hello/basicmvc/exception/servlet/ServletExController.java
@@ -0,0 +1,33 @@
+package com.hello.basicmvc.exception.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ServletExController {
+
+    // WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
+
+    @GetMapping("/error-ex")
+    public void errorEx(){
+        throw  new RuntimeException("예외발생!");
+    }
+
+
+    // sendError : 상태코드 지정
+    // WAS(sendError 호출 기록 확인) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러 (response.sendError())
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404, "404오류!");
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500, "500오류!");
+    }
+}
