52f034fdb01222570e1ecf3065193654a3dc0a2f
berryberry189 <berryberry189@naver.com>
2021-09-01 20:23:06 +0900

    ExControllerAdvice 생성

---
+++ src/main/java/com/hello/basicmvc/exception/exhandler/advice/ExControllerAdvice.java
@@ -0,0 +1,9 @@
+package com.hello.basicmvc.exception.exhandler.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExControllerAdvice {
+}
